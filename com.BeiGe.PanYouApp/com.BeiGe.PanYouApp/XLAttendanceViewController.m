be880b32ee088137276e1790312f42421ad21ea1
xiaolang <langyaoyu@hotmail.com>
2016-11-04 14:58:07 +0800

    外勤0.2

--- com.BeiGe.PanYouApp/com.BeiGe.PanYouApp/XLAttendanceViewController.m
+++ com.BeiGe.PanYouApp/com.BeiGe.PanYouApp/XLAttendanceViewController.m
@@ -7,7 +7,10 @@
 //
 
 #import "XLAttendanceViewController.h"
-
+#import "XLOutsideViewController.h"
+#import "XLLeaveViewController.h"
+#import "XLStatisticsViewController.h"
+#import "Color+Hex.h"
 @interface XLAttendanceViewController (){
     //签到标识
     int dao;
@@ -16,7 +19,10 @@
 }
 
 @end
-
+/*
+    现缺少两个网络接口
+ 
+ */
 @implementation XLAttendanceViewController
 
 - (void)viewDidLoad {
@@ -26,6 +32,30 @@
     tui=0;
     [self riqixianshi];
     [self wangluolianjie];
+    
+    [self anniupanduan];
+}
+-(void)anniupanduan{
+    //需要在接口返回时判断
+    if (dao==0) {
+        //灰色图标＋不可点击；ff9900
+        _qiandao.backgroundColor=[UIColor lightGrayColor];
+        _qiandao.userInteractionEnabled=NO;
+    }else{
+        //橙色图标＋可点击 black
+        _qiandao.backgroundColor=[UIColor colorWithHexString:@"ff9900"];
+        _qiantui.userInteractionEnabled=YES;
+    }
+    if (tui==0) {
+        //灰色图标＋不可点击；
+        _qiantui.backgroundColor=[UIColor lightGrayColor];
+        _qiantui.userInteractionEnabled=NO;
+    }else{
+        //橙色图标＋可点击
+        _qiantui.backgroundColor=[UIColor colorWithHexString:@"ff9900"];
+        _qiantui.userInteractionEnabled=YES;
+    }
+
 }
 -(void)riqixianshi{
     NSArray * arrWeek=[NSArray arrayWithObjects:@"星期日",@"星期一",@"星期二",@"星期三",@"星期四",@"星期五",@"星期六", nil];
@@ -45,34 +75,40 @@
     NSInteger month = [comps month];
     NSInteger days = [comps day];
     _day.text=[NSString stringWithFormat:@"%ld年%ld月%ld日",(long)year,month,days];
-    _week.text=[NSString stringWithFormat:@"%@",[arrWeek objectAtIndex:weeks]];
+    _week.text=[NSString stringWithFormat:@"%@",[arrWeek objectAtIndex:(weeks-1)]];
 }
 -(void)wangluolianjie{
     
 }
 - (IBAction)QianDao:(id)sender {
-    if (dao==0) {
-        //灰色图标＋不可点击；
-        
-    }else{
-        //橙色图标＋可点击
-    }
+    [self qiandao_tui:1];
 }
 
 - (IBAction)QianTui:(id)sender {
-    if (tui==0) {
-        //灰色图标＋不可点击；
-    }else{
-        //橙色图标＋可点击
-    }
+    [self qiandao_tui:2];
 }
 
 - (IBAction)WaiQin:(id)sender {
+    XLOutsideViewController*ss;
+    [self tiaoye:ss mingzi:@"outside"];
 }
 
 - (IBAction)QingJia:(id)sender {
+    XLLeaveViewController*ss;
+    [self tiaoye:ss mingzi:@"leave"];
 }
 
 - (IBAction)TongJi:(id)sender {
+    //tiaoye
+    XLStatisticsViewController*ss;
+    [self tiaoye:ss mingzi:@"statistics"];
 }
+-(void)tiaoye:(UIViewController*)controller mingzi:(NSString*)ming{
+    controller=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:ming];
+    [self.navigationController pushViewController:controller animated:YES];
+}
+-(void)qiandao_tui:(int)nage{
+    
+}
+
 @end
